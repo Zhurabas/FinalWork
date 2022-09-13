f66d773936529e38ea2c30b1f6c48047eb196642
lewe0fun <pakuln.yury@gmail.com>
2022-09-13 19:56:00 +0300

    задча - метод printStringArr

--- Methods/mtd.cs
+++ Methods/mtd.cs
@@ -3,5 +3,15 @@ using static System.Console;
 namespace Methods;
 public class mtd
 {
-
+public static void printStringArr(string[]arr)
+{
+    Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        for (int j = 0; j < arr[i].Length; j++)
+            Write($"{arr[i][j]}");
+       if(i!=arr.Length-1) Write(", ");
+    }
+    Write("]");
+}
 }
